f46b4e6b7ca3fc32a807a2ff735e90061e1a3381
Antoine Stoecklin <astoeck@hotmail.fr>
2023-09-12 17:46:52 +0200

    frist change exo 1

--- shop.pizza-shop/src/domain/service/CommandeService.php
+++ shop.pizza-shop/src/domain/service/CommandeService.php
@@ -2,7 +2,38 @@
 
 namespace pizzashop\shop\domain\service;
 
-class CommandeService
-{
+use Exception;
+use pizzashop\tests\commande\ServiceCommandeTest;
 
+class CommandeService extends Exception
+{
+    private string $id;
+    private $date;
+    private int $type;
+    private float $totalPrice;
+    private int $state;
+    private string $mail;
+
+    public function __construct(string $id, $date, int $type, float $totalPrice, int $state, string $mail)
+    {
+        $this->id = $id;
+        $this->date = $date;
+        $this->type = $type;
+        $this->totalPrice = $totalPrice;
+        $this->state = $state;
+        $this->mail = $mail;
+    }
+
+    public function readCommand(string $id){
+            if ($id =  true) {
+                return $id;
+            }
+            else {
+                throw new Exception(ServiceCommandNotFound());
+            }
+    }
+
+    public function validateCommand(string $id){
+
+    }
 }
