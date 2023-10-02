2d5f2ed2c7c7b1b98b3b704ac83e38647da14c09
CreepERROR <benjamin.hugot@outlook.fr>
2023-10-02 09:32:48 +0200

    Ajout Exception

---
+++ shop.pizza-shop/src/domain/service/exception/ServiceCommandeNotFoundException.php
@@ -0,0 +1,7 @@
+<?php
+
+class ServiceCommandeNotFoundException extends Exception {
+    public function __construct($message = "Service de commande introuvable", $code = 0, Exception $previous = null) {
+        parent::__construct($message, $code, $previous);
+    }
+}
