2432b7a94464fdfce477c7a43ec6512a941fc5de
Antoine Stoecklin <astoeck@hotmail.fr>
2023-10-03 15:48:04 +0200

    correction

--- shop.pizza-shop/src/routes/accessCommande.php
+++ shop.pizza-shop/src/routes/accessCommande.php
@@ -5,8 +5,6 @@ namespace pizzashop\shop\routes;
 use Illuminate\Database\Eloquent\Model;
 use PDOException;
 use pizzashop\shop\models\Command;
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
 use Slim\App;
 use Slim\Factory\AppFactory;
 
