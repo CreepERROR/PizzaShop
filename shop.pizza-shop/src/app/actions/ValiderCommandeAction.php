65817968f9b1ea201e7134b21ab42112ee9d4bba
Antoine Stoecklin <astoeck@hotmail.fr>
2023-10-03 16:49:17 +0200

    valider commande

---
+++ shop.pizza-shop/src/app/actions/ValiderCommandeAction.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace pizzashop\shop\app\actions;
+
+use pizzashop\shop\domain\service\command\CommandService;
+use pizzashop\shop\domain\service\exception\CommandeNotFoundException;
+use Slim\Exception\HttpInternalServerErrorException;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+
+class AccederCommandeAction
+{
+    public function __invoke(Request $request, Response $response, $args): Response
+    {
+        $id = $args['id_commande'];
+        $serviceCommande = new CommandService();
+        try {
+            $commande = $serviceCommande->readCommand($id);
+        } catch (CommandeNotFoundException $e) {
+            throw new HttpInternalServerErrorException($request, $e->getMessage());
+        }
+
+        $commandeFormated = [
+            $commande,
+            'links' => [
+                'commandes' => [
+                    'href' => '/commandes' // Utilisez l'URL relative directement
+                ]
+            ],
+        ];
+
+        $dataFormated = [
+            'type' => 'ressource',
+            'commande' => $commandeFormated
+        ];
+
+        // Convertir le tableau en JSON
+        $json = json_encode($dataFormated);
+
+        // Ajouter le contenu JSON à la réponse
+        $response->getBody()->write($json);
+
+        // Définir le type de contenu de la réponse comme JSON
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+}
