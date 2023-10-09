a00bc3dc64fc01c30cafdb32393ff0ca9af13f8e
DELLINGER Agathe <agathe.dellinger2@etu.univ-lorraine.fr>
2023-10-09 23:36:20 +0200

    base TD4ex5

---
+++ auth.pizza-shop/src/app/actions/SignInAction.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace pizzashop\auth\api\actions;
+
+//manque use pr le service et pr l'exception
+// TODO: Ajouter les use nécessaires
+use pizzashop\auth\api\actions\AbstractAction;
+use Slim\Exception\HttpInternalServerErrorException;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+
+class SignInAction extends AbstractAction
+{
+    public function __invoke(Request $request, Response $response, $args): Response
+    {
+        //TODO: Ajouter le code de l'action Signin Auth
+    }
+}
