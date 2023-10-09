efdc1be22483d611803ca12f2a6336b74f485264
Antoine Stoecklin <astoeck@hotmail.fr>
2023-10-09 09:29:59 +0200

    class authservice done imo

--- shop.pizza-shop/src/domain/service/authentification/authService.php
+++ shop.pizza-shop/src/domain/service/authentification/authService.php
@@ -8,34 +8,48 @@ use Firebase\JWT\ExpiredException;
 use Firebase\JWT\SignatureInvalidException ;
 use Firebase\JWT\BeforeValidException;
 
-$secret = $_ENV['SECRET_KEY']; // secret key
+class authService {
 
+    private $secret;
+    private $entete;
+    private $payload;
+    private $token;
 
-$entete = [
-    "alg" => "HS512", // hashing
-    "typ" => "JWT" // type
-];
+    public function __construct ($secret,$entete,$payload, $token){
 
-
-$payload = [
-    "iss" => "http://localhost:8080/", // issuer, émetteur du token
-    "sub" => "pizza-shop.db", // Subject
-    "aud" => "pizzashopcomponents-api.pizza-auth-1",//audience, utilisateur du token
-    "iat" => time(), // Heure d'émission
-    "exp" => time() + 3600 // Heure d'expiration
-];
-
-$token = JWT::encode($payload, $secret, 'HS512');
-
-try {
-    $h = $rq->getHeader('Authorization')[0] ;
-    $tokenstring = sscanf($h, "Bearer %s")[0] ;
-    $token = JWT::decode ($tokenstring, new Key($secret,'HS512' ));
+        $this->secret=$_ENV['SECRET_KEY'];
+        $this->entete= [
+            "alg" => "HS512", // hashing
+            "typ" => "JWT" // type
+        ];;
+        $this->payload=[
+            "iss" => "http://localhost:8080/", // issuer, émetteur du token
+            "sub" => "pizza-shop.db", // Subject
+            "aud" => "pizzashopcomponents-api.pizza-auth-1",//audience, utilisateur du token
+            "iat" => time(), // Heure d'émission
+            "exp" => time() + 3600 // Heure d'expiration
+        ];
+        $this->token= JWT::encode($payload, $secret, 'HS512');
     }
 
-    catch (ExpiredException $e) {} 
-    catch (SignatureInvalidException $e) {} 
-    catch (BeforeValidException $e) {} 
-    catch (\UnexpectedValueException $e) { }
+    public function decodeAccessToken($secret,$rq){
+
+        try {
+        $h = $rq->getHeader('Authorization')[0] ;
+        $tokenstring = sscanf($h, "Bearer %s")[0] ;
+        $token = JWT::decode ($tokenstring, new Key($secret,'HS512' ));
+        return $token;
+        }
+    
+        catch (ExpiredException $e) {} 
+        catch (SignatureInvalidException $e) {} 
+        catch (BeforeValidException $e) {} 
+        catch (\UnexpectedValueException $e) { }
+
+    }       
+
+}
+
+
 
 ?>
