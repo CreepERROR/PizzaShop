9dff3dd9195dfb7eb79b614c86c238183d174cbf
Antonin <anto10800@gmail.com>
2023-10-17 15:23:08 +0200

    maj

--- auth.pizza-shop/config/bootstrap.php
+++ auth.pizza-shop/config/bootstrap.php
@@ -1,49 +1,37 @@
 <?php
 
 use DI\ContainerBuilder;
+use Slim\Factory\AppFactory as Factory;
 use Illuminate\Database\Capsule\Manager as Eloquent;
-use Slim\Factory\AppFactory;
-use Slim\Views\Twig;
-use Slim\Views\TwigMiddleware;
-
-$builder = new DI\ContainerBuilder();
-
-try {
-   $settings = require_once __DIR__ . '/settings.php';
-//    $dependencies = require_once __DIR__.'/services_dependencies.php';
-//    $actions= require_once __DIR__.'/actions_dependencies.php';
-    $builder = new ContainerBuilder();
-//    $builder->addDefinitions($settings );
-//    $builder->addDefinitions($dependencies);
-//    $builder->addDefinitions($actions);
-    $c = $builder->build();
-    $app = AppFactory::createFromContainer($c);
-    $container = $app->getContainer();
-} catch (Exception $e) {
-    echo $e->getMessage();
-    exit();
-}
-
-$app->addRoutingMiddleware();
-$app->addErrorMiddleware(true, false, false);
-$twig = Twig::create('../src/templates',['cache'=>'cache/','auto_reload'=>true]);
-
-$eloquent = new Eloquent();
-$eloquent->addConnection(parse_ini_file(__DIR__ . '/auth.db.ini'), 'auth');
-$eloquent->setAsGlobal();
-$eloquent->bootEloquent();
-
-$app->add(TwigMiddleware::create($app,$twig));
 
 //$settings = require_once __DIR__ . '/settings.php';
-//$dependencies = require_once __DIR__.'/services_dependencies.php';
-//$actions= require_once __DIR__.'/actions_dependencies.php';
-//
-//$builder = new ContainerBuilder();
-//$builder->addDefinitions($settings );
-//$builder->addDefinitions($dependencies);
-//$builder->addDefinitions($actions);
-//$c=$builder->build();
-//$app = AppFactory::createFromContainer($c);
+$dependencies = require_once __DIR__ . '/dependencies.php';
+$actions = require_once __DIR__ . '/actions.php';
+
+$builder = new ContainerBuilder();
+//$builder->addDefinitions($settings);
+$builder->addDefinitions($dependencies);
+$builder->addDefinitions($actions);
+try {
+    $c = $builder->build();
+} catch (Exception $e) {
+}
+$app = Factory::create();
+
+
+$app->addBodyParsingMiddleware();
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(true, false, false)
+    ->getDefaultErrorHandler()
+    ->forceContentType('application/json');
+
+$eloquent = new Eloquent();
+   $eloquent ->addConnection(parse_ini_file(__DIR__ . '/auth.db.ini'), 'auth');
+
+
+(require_once __DIR__ . '/routes.php')($app);
+$routeParser = $app->getRouteCollector()->getRouteParser();
+
+
 
 return $app;
--- auth.pizza-shop/public/index.php
+++ auth.pizza-shop/public/index.php
@@ -7,8 +7,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 /* application boostrap */
 $app = require_once __DIR__ . '/../config/bootstrap.php';
 
-/* Routes loading */
-(require_once __DIR__ . '/../config/routes.php')($app);
 
 $app->run();
 
