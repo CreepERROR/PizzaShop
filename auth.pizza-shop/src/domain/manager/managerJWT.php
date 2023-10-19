7398d7d940fbad21c614df29ae4093a6d7504aae
Antoine Stoecklin <astoeck@hotmail.fr>
2023-10-19 15:04:25 +0200

    create and validate token

--- auth.pizza-shop/src/domain/manager/managerJWT.php
+++ auth.pizza-shop/src/domain/manager/managerJWT.php
@@ -9,12 +9,46 @@ use Firebase\JWT\BeforeValidException;
 
 class managerJWT implements IManagerJWT
 {
+    private $secret;
+    private $entete;
+    private $payload;
+    private $token;
+    
     public function __construct()
-    {}
+    {
+        $this->secret=$_ENV['SECRET_KEY'];
+        $this->entete= [
+            "alg" => "HS512", // hashing
+            "typ" => "JWT" // type
+        ];;
+        $this->payload=[
+            "iss" => "http://localhost:8080/", // issuer, émetteur du token
+            "sub" => "pizza-shop.db", // Subject
+            "aud" => "pizzashopcomponents-api.pizza-auth-1",//audience, utilisateur du token
+            "iat" => time(), // Heure d'émission
+            "exp" => time() + 3600 // Heure d'expiration
+        ];
+    }
 
     public function createToken($data)
     {
-        // TODO: Implement createToken() method.
+    $users = Users::all();
+
+    foreach ($users as $user){
+        $payload = [
+            "iss" => $this->payload['iss'],
+            "sub" => $data['sub'],
+            "aud" => $this->payload['aud'],
+            "iat" => $this->payload['iat'],
+            "exp" => $this->payload['exp']
+        ];
+    
+        // Encodez le jeton JWT avec les données utilisateur
+        $token = JWT::encode($user,$_ENV['SECRET_KEY'] , 'HS512');
+    
+        return $token;
+    }
+    
     }
 
     public function validateToken($token)
