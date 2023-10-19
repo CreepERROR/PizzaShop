40862b6abad91676f8161e0b0264ef5eff9501a3
DELLINGER Agathe <agathe.dellinger2@etu.univ-lorraine.fr>
2023-10-19 14:25:35 +0200

    test

---
+++ auth.pizza-shop/src/app/actions/TestAuthAction.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace pizzashop\auth\api\app\actions;
+
+//manque use pr le service et pr l'exception
+// TODO: Ajouter les use nécessaires
+use pizzashop\auth\api\app\actions\AbstractAction;
+use Slim\Exception\HttpInternalServerErrorException;
+
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+use pizzashop\auth\api\domain\service\ServiceAuth;
+use pizzashop\auth\api\domain\provider\Provider;
+use pizzashop\auth\api\domain\manager\ManagerJWT;
+
+class TestAuthAction extends AbstractAction
+{
+    public function __invoke(Request $request, Response $response, $args): Response
+    {
+        $body = $response->getBody()->write("blablabla");
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+}
