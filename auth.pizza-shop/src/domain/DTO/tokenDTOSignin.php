36b6207649c01247641c01fda0b091f0ecbe6408
Antoine Stoecklin <astoeck@hotmail.fr>
2023-10-20 11:41:26 +0200

    secret fired

--- auth.pizza-shop/src/domain/DTO/tokenDTOSignin.php
+++ auth.pizza-shop/src/domain/DTO/tokenDTOSignin.php
@@ -4,13 +4,11 @@ namespace pizzashop\auth\api\domain\DTO\tokenDTOsignin;
 
 class tokenSigninDTO{
 
-    private $secret;
     private $entete;
     private $payload;
 
     public function __construct($secret,$entete,$payload)
     {
-        $this->secret = $secret;
         $this->entete = $entete;
         $this->payload = $payload;
     }
