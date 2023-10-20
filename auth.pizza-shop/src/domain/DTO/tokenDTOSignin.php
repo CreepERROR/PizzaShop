2af38a1971cac41b2a7bb3441eed7e34f1d2fb50
Antoine Stoecklin <astoeck@hotmail.fr>
2023-10-20 11:06:33 +0200

    tokensigninDTO

--- auth.pizza-shop/src/domain/DTO/tokenDTOSignin.php
+++ auth.pizza-shop/src/domain/DTO/tokenDTOSignin.php
@@ -1,1 +1,18 @@
+<?php
+
+namespace pizzashop\auth\api\domain\DTO\tokenDTOsignin;
+
+class tokenSigninDTO{
+
+    private $secret;
+    private $entete;
+    private $payload;
+
+    public function __construct($secret,$entete,$payload)
+    {
+        $this->secret = $secret;
+        $this->entete = $entete;
+        $this->payload = $payload;
+    }
+}
 
