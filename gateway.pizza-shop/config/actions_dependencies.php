c0a50af4f0d6567d53431577d92c75b53ea7af4d
Antonin <anto10800@gmail.com>
2024-01-08 11:40:02 +0100

    Architecture ajout API Gateway

--- gateway.pizza-shop/config/actions_dependencies.php
+++ gateway.pizza-shop/config/actions_dependencies.php
@@ -10,11 +10,10 @@ return[
     'guzzle.client' => function(\Psr\Container\ContainerInterface $c){
         return new \GuzzleHttp\Client(
             [
-                'base_uri' => 'api.pizza-shop',
+                'base_uri' => 'http://api.pizza-shop',
                 'headers' => [
                     'Accept' => 'application/json',
-                    'Origin' => '',
-                    'Authorization' => 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwODAvIiwic3ViIjoicGl6emEtc2hvcC5kYiIsImF1ZCI6InBpenphc2hvcGNvbXBvbmVudHMtYXBpLnBpenphLWF1dGgtMSIsImlhdCI6MTcwNDY2MjA0NCwiZXhwIjoxNzA0NjY1NjQ0LCJ1c2VybmFtZSI6IkFsaXhQZXJyb3QifQ.EvCNS4Nnj4KrNoWYlkboTuy4wyiwAxjMDRuNHQbDvGY'
+                    'Origin' => '*'
                 ]
             ]
         );
