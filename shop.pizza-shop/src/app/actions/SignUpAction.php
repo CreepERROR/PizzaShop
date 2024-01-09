573d45b9dd9cc4a173b0a3f5affe1f9972a03a63
CreepERROR <benjamin.hugot@outlook.fr>
2024-01-09 17:40:19 +0100

    Add Signin to Gateway

---
+++ shop.pizza-shop/src/app/actions/SignUpAction.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace pizzashop\shop\app\actions;
+use GuzzleHttp\Client;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+use GuzzleHttp\Psr7\Response as GuzzleResponse;
+
+class SignUpAction extends AbstractAction
+{
+    public function __invoke(Request $request, Response $response, $args): Response
+    {
+        try {
+            $body = $request->getBody()->getContents();
+
+            $client = new Client([
+                'base_uri' => 'http://api.pizza-auth',
+                'timeout' => 15.0,
+            ]);
+            $response = $client->request('POST', '/api/users/signup', [
+                'headers' => [
+                    'Authorization' => $request->getHeader('Authorization')
+                ],
+                'body' => $body
+            ]);
+            $code = $response->getStatusCode();
+            if ($code != 200) {
+                throw new \Exception('Inscription invalide');
+            } else {
+                $body = $response->getBody()->getContents();
+                $response = new Response();
+                $response->getBody()->write($body);
+            }
+        }catch (\Error $e){
+            $response->withStatus(400);
+            $response->getBody()->write($e->getMessage());
+        }
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+}
