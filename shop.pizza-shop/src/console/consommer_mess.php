cb41cb7950782cba013e0904cd65523fe93fc5ff
DELLINGER Agathe <agathe.dellinger2@etu.univ-lorraine.fr>
2024-01-17 10:18:21 +0100

    ex2 okok

--- shop.pizza-shop/src/console/consommer_mess.php
+++ shop.pizza-shop/src/console/consommer_mess.php
@@ -5,16 +5,20 @@ use \PhpAmqpLib\Connection\AMQPStreamConnection;
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 $message_queue = 'nouvelles_commandes';
-$connection = new AMQPStreamConnection('rabbitmq', 5672, 'admin', 'admin');
+try {
+    $connection = new AMQPStreamConnection('rabbitmq', 5672, 'admin', 'admin');
+} catch (Exception $e) {
+    print "[x] Erreur de connexion : " . $e->getMessage() . "\n";
+    //exit(1);
+}
 $channel = $connection->channel();
 $msg = $channel->basic_get($message_queue);
 if ($msg) {
     $content = json_decode($msg->body, true);
-    print "[x] message reçu : \n" ;
+    print "[x] commande bien acquittée \n" ;
     $channel->basic_ack($msg->getDeliveryTag());
-    print "\n";
 } else {
-    print "[x] pas de message reçu\n"; exit(0);
+    print "[x] pas de commandes\n"; exit(0);
 }
 $channel->close();
 $connection->close();
