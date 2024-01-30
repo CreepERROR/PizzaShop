48adb72b92aae36206c47745dd7207612e1f417c
Antoine Stoecklin <astoeck@hotmail.fr>
2024-01-30 16:56:58 +0100

    commandes services done, need to try

--- service-dir/src/services/CommandeService.js
+++ service-dir/src/services/CommandeService.js
@@ -1,15 +1,17 @@
-import ('../models/')
+import knex from 'knex';
+
+import config from "./knexfile";
+
+const db = knex(config.development);
 
 async function getCommandes() {
-    return await Commande.findAll();
+  try {
+    const result = await db.select('id', 'date_commande', 'delai', 'montant_total', 'mail_client', 'type_livraison').from('Commande');
+    return result;
+  } catch (err) {
+    console.error(err);
+    throw new Error("Impossible d'accéder aux commandes");
+  }
 }
 
-async function getCommande(id) {
-    return await Commande.findOne({
-        where: {
-            id: id
-        }
-    });
-}
-
-export {getCommandes, getCommande}
+export { getCommandes };
