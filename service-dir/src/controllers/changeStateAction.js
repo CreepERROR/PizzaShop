395f7b2a943208a57909b8b521f84304147034c3
Antoine Stoecklin <astoeck@hotmail.fr>
2024-02-02 15:10:13 +0100

    exo 2 td 9 & started exo 1 td 10

--- service-dir/src/controllers/changeStateAction.js
+++ service-dir/src/controllers/changeStateAction.js
@@ -1,13 +1,15 @@
-import { changeState } from '../services/CommandeService.js';
 
 export default async function changeStateAction(req, res, next) {
     try{
-        const commandeId = req.params.id;
-        const state = req.body.state;
-        const changed = await changeState(commandeId, state);
-        res.json(changed);
+        const { id } = req.params;
+        const { newState } = req.body;
+        const message = JSON.stringify({ id: parseInt(id), newState });
+
+        await publishMessage(message);
+    
+        return res.status(200).json({ success: true });
     }catch (e){
-        next(404);
+        next(res.status(400).json({ error: 'L\'identifiant de la commande et le nouvel état sont requis.' }));
     }
 
 }
