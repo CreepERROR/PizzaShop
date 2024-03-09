e514ddc5647429dfa69663d6af52e76430725d73
CreepERROR <benjamin.hugot@outlook.fr>
2024-03-09 14:29:15 +0100

    TD9 étape 2 exo 2

--- service-dir/src/services/MessagesService.js
+++ service-dir/src/services/MessagesService.js
@@ -11,6 +11,29 @@ export default class MessagesService {
   constructor() {
     this.#_service = new CommandeService();
   }
+  async createSuiviQueue(channel) {
+    const suiviExchange = 'pizzashop';
+    const suiviQueue = 'suivi_commandes';
+    const suiviRoutingKey = 'suivi';
+    await channel.assertQueue(suiviQueue);
+    await channel.bindQueue(suiviQueue, suiviExchange, suiviRoutingKey);
+  }
+  async publishMessageToSuivi(data) {
+    try {
+      const suiviExchange = 'pizzashop';
+      const suiviRoutingKey = 'suivi';
+
+      const conn = await amqp.connect(rabbitmq);
+      const channel = await conn.createChannel();
+      await channel.assertExchange(suiviExchange, 'direct', { durable: true });
+      await this.createSuiviQueue(channel);
+      await channel.publish(suiviExchange, suiviRoutingKey, Buffer.from(JSON.stringify(data)));
+    } catch (err) {
+      console.error(err);
+      throw new Error("Unable to connect to RabbitMQ");
+    }
+  }
+
   async messageEnCommande() {
     try {
       const exchange = 'pizzashop';
@@ -22,6 +45,10 @@ export default class MessagesService {
         //console.log('msg content: ' + msg.content);
         let data = JSON.parse(msg.content);
         this.#_service.createCommand(data.type_livraison, data.montant_total, data.mail_client)
+        this.publishMessageToSuivi({
+          commandId: data.commandId,
+          newStatus: data.newStatus,
+        });
         //ack() = used to acknowledge that the particular message has been received by the ‘client-app’.
         channel.ack(msg);
       })
